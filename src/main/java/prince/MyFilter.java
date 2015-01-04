e0c7979a75625401ab62ac3f3c18ab2ae5464899
yhjhoo <yhjhoo@163.com>
2015-01-04 23:02:58 +0800

    init

--- src/main/java/prince/MyFilter.java
+++ src/main/java/prince/MyFilter.java
@@ -16,7 +16,7 @@ import org.apache.commons.logging.LogFactory;
 /**
  * Servlet Filter implementation class MyFilter
  */
-@WebFilter("/MyFilter")
+@WebFilter("/*")
 public class MyFilter implements Filter {
 
 	private Log log = LogFactory.getLog(this.getClass());
@@ -44,6 +44,16 @@ public class MyFilter implements Filter {
 		// TODO Auto-generated method stub
 		// place your code here
 		log.info("doFilter");
+		
+		log.debug("doFilter debug: test");
+		
+		log.info("doFilter info: test");
+		
+		log.warn("doFilter warn: test");
+		
+		log.error("doFilter error: test");
+		
+		log.fatal("doFilter fatal: test");
 
 		// pass the request along the filter chain
 		chain.doFilter(request, response);
